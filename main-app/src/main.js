5cda7905365298fb1245eea0f3a9a0ec4c551656
zhaoliang <kybetter@163.com>
2019-10-29 17:27:25 +0800

    feat

--- main-app/src/main.js
+++ main-app/src/main.js
@@ -8,47 +8,8 @@ import Framework from './Framework.vue';
 // import systemjs from 'systemjs/dist/extras/amd';
 // import importHTML,{ importEntry } from 'import-html-entry';
 import fetch from 'isomorphic-fetch';
-// import axios from 'axios';
 
-// import fetch from 'isomorphic-fetch';
-// import Vue from 'vue';
 import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start } from './dist/index.esm.js';
-// const request = url =>
-//   fetch(url, {
-//     referrerPolicy: 'origin-when-cross-origin',
-//   });
-
-// async function r() {
-//   const { template: appContent, execScripts } = await importEntry('//localhost:8089/sub.html', { 
-//       fetch: request
-//     });
-
-//     // console.log(appContent);
-//     console.log(execScripts);
-//     const { bootstrap: bootstrapApp, mount, unmount } = await execScripts(false);
-//     console.log(bootstrapApp);
-//     console.log(mount);
-//     console.log(unmount);
-// }
-
-// r();
-
-// importHTML('//localhost:8089/sub.html').then(res => {
-//   console.log(res);
-//   console.log(res.getExternalScripts().then(res => {
-//     console.log(res);
-//   }));
-// })
-
-// new Vue({
-//     render: (h) => h(App),
-//   router,
-//   store,
-//   el: '#app'
-// })
-
-
-// import Framework from './Framework.vue';
 
 let app = null;
 
@@ -86,62 +47,7 @@ function genActiveRule(routerPrefix) {
 
 render({ loading: true });
 
-// function loadModule(module) {
-//   return new Promise(resolve => {
-//     axios.get(module).then(res => {
-//       // console.log(res.data);
-//       console.log(res.data);
-//       resolve(require(eval(res.data)));
-//     })
-//   }, reject => {
-
-//   })
-//   // return axios.get(module)
-// }
-// loadModule('http://localhost:8080/js/app.js')
-
-// console.log(systemjs);
-
 Vue.config.productionTip = false;
-// new Vue({
-//   render: (h) => h(App),
-//   router,
-//   store,
-//   el: '#app'
-// });
-// const vueLifecycles = singleSpaVue({
-//   Vue,
-//   appOptions: {
-//     el: '#app',
-//     render: (h) => h(App),
-//     router,
-//     store,
-//   },
-// });
-
-// export const bootstrap = vueLifecycles.bootstrap;
-// export const mount = vueLifecycles.mount;
-// export const unmount = vueLifecycles.unmount;
-
-// registerApplication(
-//   'child', 
-//   // loadModule('http://localhost:8080/js/app.js?a=12312312'),
-//   require('../../child-app/src/main'),
-//   // async () => {
-//   //   const { template: appContent, execScripts } = await importEntry('//localhost:8089', { 
-//   //     fetch: request
-//   //   });
-
-//   //   console.log(appContent);
-//   //   console.log(execScripts);
-//   // },
-//   // () => systemjs.import('child'),
-//   // () => systemjs.import('http://localhost:8089/sub.app.js'),
-//   location => true
-// )
-
-// start();
-
 
 const request = url =>
   fetch(url, {
